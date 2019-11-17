948345105d6485aab6072fcc81da405c965c6428
Nick Rease <narease20@transy.edu>
2019-11-17 16:06:55 -0500

    added Random

---
+++ Random.h
@@ -0,0 +1,12 @@
+/*Fran, Perry, Nick 
+  Fall 2019
+  CS3074 Project 4
+  FILE: Random.h
+  Randomizes what page to replace
+  Takes in a index size and returns an index number to replace
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getRandom (int indexSize);
