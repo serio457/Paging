2c3cf4b6f355f97fc6f956393a841ae2967b0bb3
Francesco Serio <fserio@lab1.transy.edu>
2019-11-20 15:24:25 -0500

    refactored. Algs are more modular and cleaner looking

--- FIFO.c
+++ FIFO.c
@@ -1,1 +1,32 @@
-FIFO(table, pageNum, firstIn)
+#include "FIFO.h"
+#include <stdio.h>
+
+int FIFO(PAGETABLE *table, PAGE memoryLocations[], int numMemLocations, int pagesize)
+{
+    int firstIn = 0, pageFaults = 0;
+    PAGE pageNum;
+    for (int j = 0; j < numMemLocations; j++)
+    {
+        pageNum = memoryLocations[j]/pagesize;
+        if (!tableCheck(*table, pageNum))
+        {
+            for (int i = 0; i < table->size; i++)
+            {
+                if (!(table->frames[i].validBit) && !(tableCheck(*table, pageNum)))
+                {
+                    pageFault(&table->frames[i], pageNum);
+                    table->frames[i].validBit = TRUE;
+                    printf("on MemLoc %d\n", pageNum);
+                }
+            }
+            pageFault(&table->frames[firstIn], pageNum);
+            pageFaults++;
+            firstIn++;
+            if (firstIn == table->size)
+            {
+                firstIn = 0;
+            }
+        }
+    }
+    return pageFaults;
+}
---
+++ FIFO.h
@@ -0,0 +1,9 @@
+#include "pageTable.h"
+
+#ifndef FIFO_H
+#define FIFO_H
+
+
+int FIFO(PAGETABLE *table, PAGE memoryLocations[], int numMemLocations, int pagesize);
+
+#endif
